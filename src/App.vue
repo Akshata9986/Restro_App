efca26554ef762c6f32040240f9ac0d5f2bc008e
Akshata9986 <akshatanaik9164@gmail.com>
2022-09-24 12:29:41 +0530

    uploading restro_app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Little Indian Restro</h1>
+  <ul>
+    <dish-details
+      v-for="dish in dishes"
+      :key="dish.id"
+      :id="dish.id"
+      :name="dish.name"
+      :img="dish.img"
+      :description="dish.description"
+      :price="dish.price"
+      :isFavourite="dish.isFavourite"
+      @toggleFavouriteStatus="toggleFavouriteStatus"
+    />
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DishDetails from "./components/DishDetails.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { DishDetails },
+  data() {
+    return {
+      dishes: [
+        {
+          id: "avi",
+          name: "Butter Chicken",
+          img: "https://media.istockphoto.com/photos/murgh-makhani-butter-chicken-tikka-masala-picture-id1262857001?b=1&k=20&m=1262857001&s=170667a&w=0&h=LDytMWvxGKBs63GhCuAxxYnG1MDibHLmNH8mij4gkPA=",
+          description: "It's finger licking good",
+          price: 350,
+          isFavourite: true,
+        },
+        {
+          id: "vaibhav",
+          name: "Chole Bhature",
+          img: "https://media.gettyimages.com/photos/closeup-chole-bhature-served-at-table-picture-id552154821?s=612x612",
+          description: "The crispy puffed bhatura and gravy",
+          price: 150,
+          isFavourite: false,
+        },
+        {
+          id: "akshata",
+          name: "Fish Fry",
+          img: "https://media.istockphoto.com/photos/tasty-grilled-fish-picture-id980462262?b=1&k=20&m=980462262&s=170667a&w=0&h=Xj7Oxu5kA57vZMXaWD917tuhpJ7JAII8G1sPKtyOT-U=",
+          description: "includes french fries,coleslaw",
+          price: 300,
+          isFavourite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    //to get dishDetails from child component
+    toggleFavouriteStatus(dishID) {
+      // console.log("dish ID in parent", dishID);
+      let identifiedDish = this.dishes.find((dish) => dish.id === dishID);
+      identifiedDish.isFavourite = !identifiedDish.isFavourite;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +69,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app h1 {
+  background-color: blueviolet;
 }
 </style>
---
+++ src/components/DishDetails.vue
@@ -0,0 +1,73 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite ? "🤤" : "" }}</h2>
+    <button @click="toggleFavourite" style="color: blue">
+      Toggle Favourite
+    </button>
+    <button @click="toggleDetails" style="color: navy blue">
+      {{ displayDetails ? "Hide" : "Show" }}Details
+    </button>
+    <ul v-if="displayDetails">
+      <li><strong>Description:</strong>{{ description }}</li>
+      <li><strong>Price:</strong>{{ price }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  emits: ["toggleFavouriteStatus"],
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      //   dishIsFavourite: this.isFavourite,
+      displayDetails: false,
+    };
+  },
+  methods: {
+    toggleFavourite() {
+      //   if (this.dishIsFavourite) {
+      //     this.dishIsFavourite = false;
+      //   } else {
+      //     this.dishIsFavourite = true;
+      //   }
+      //   this.dishIsFavourite = !this.dishIsFavourite;
+      this.$emit("toggleFavouriteStatus", this.id);
+    },
+    toggleDetails() {
+      this.displayDetails = !this.displayDetails;
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  list-style-type: none;
+}
+</style>
